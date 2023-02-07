f9daeeb09b41dbeb34ae030b2e41f0271d517701
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-02-07 11:43:14 +0100

    opdraccht 1

--- module-1/m01o1/m01o1.js
+++ module-1/m01o1/m01o1.js
@@ -3,7 +3,12 @@ let uitleg = "In een parkeergarage passen 80 auto's op de begane grond en 120 op
 
 alert(uitleg);
 let gewenste_aantal_autos = prompt("Hoeveel autos wilt u kwijt in de parkeergarage?");
-// Berekenen hier het aantal verdiepingen
+
+let berekening = Math.ceil((gewenste_aantal_autos - 80) / 120);
+document.getElementById("berekening").innerText = berekening;
+console.log(berekening)
+window.alert(berekening)
+alert(berekening)
 
 let antwoord = "Ik heb nog geen idee hoeveel verdiepingen er moeten komen :-( ";
 document.getElementById("antwoord").innerText = antwoord;
