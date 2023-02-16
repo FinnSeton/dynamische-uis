d2e6e3b30c71375e6fb72147c20a7813b349feb8
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-02-16 12:00:38 +0100

    .

--- module-1/HorecaApp/HorecaApp.js
+++ module-1/HorecaApp/HorecaApp.js
@@ -1,3 +1,6 @@
+const PRIJS_FRIS = 2.00;
+const PRIJS_BIER = 2.50;
+const PRIJS_WIJN = 3.00;
 var bonnetje = [];
 
 while (true) {
@@ -26,9 +29,23 @@ while (true) {
 }
 
 if (bonnetje.length > 0) {
-    var bonnetjeOutput = "Bonnetje:\n";
+    var bonnetjeOutput = "Bonnetje:<br>";
+    var totaalprijs = 0;
     for (var i = 0; i < bonnetje.length; i++) {
-        bonnetjeOutput += bonnetje[i].aantal + " " + bonnetje[i].product + "\n";
+        var product = bonnetje[i].product;
+        var aantal = bonnetje[i].aantal;
+        var prijs = 0;
+        if (product === "fris") {
+            prijs = PRIJS_FRIS;
+        } else if (product === "bier") {
+            prijs = PRIJS_BIER;
+        } else if (product === "wijn") {
+            prijs = PRIJS_WIJN;
+        }
+        var productTotaal = prijs * aantal;
+        bonnetjeOutput += aantal + " " + product + " (" + prijs.toFixed(2) + " euro/stuk) = " + productTotaal.toFixed(2) + " euro<br>";
+        totaalprijs += productTotaal;
     }
-    alert(bonnetjeOutput);
+    bonnetjeOutput += "Totaal: " + totaalprijs.toFixed(2) + " euro";
+    document.getElementById("output").innerHTML = bonnetjeOutput;
 }
