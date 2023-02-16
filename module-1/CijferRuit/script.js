47b4d22dbf12233e45d7cd52a7a5abb9c83a0cb4
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-02-16 12:04:49 +0100

    Update script.js

--- module-1/CijferRuit/script.js
+++ module-1/CijferRuit/script.js
@@ -1,8 +1,8 @@
-s
+let aantal = prompt("Welk cijfer wil je voor je cijferruit?");
 let list = "";
-for (let number = 1; number <= aantal; number++) {
+for (let number = 1; number <= aantal; number++){
     list += number;
-    document.getElementById("antwoord").innerText += list + '\n';
+    document.getElementById("antwoord").innerText += list+ '\n';
     list = number === parseInt(aantal) ? list += "" : list += "-";
 }
 for (let number = aantal; number >= 1; number--) {
