e9a1df9ac8e6d0eca15f2852234f0e0e798a1526
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-03-14 12:14:46 +0100

    .

--- module-2/Button 1.0/buttons-template/buttons.js
+++ module-2/Button 1.0/buttons-template/buttons.js
@@ -1,17 +1,20 @@
-
-
 let buttonValues = [0, 0, 0];
 
 function buttonClicked(buttonIndex) {
-  buttonValues[buttonIndex-1] = 1;
-
   if (buttonIndex == 1) {
+    buttonValues[buttonIndex - 1] += 1
+    document.getElementById('button1').innerHTML = buttonValues[buttonIndex - 1]
     document.getElementById("img1").src = "images/bg1.jpg";
     document.getElementById("img2").src = "images/1.jpg";
+
   } else if (buttonIndex == 2) {
+    buttonValues[buttonIndex - 1] += 1
+    document.getElementById('button2').innerHTML = buttonValues[buttonIndex - 1]
     document.getElementById("img1").src = "images/bg2.jpg";
     document.getElementById("img2").src = "images/2.jpg";
   } else if (buttonIndex == 3) {
+    buttonValues[buttonIndex - 1] += 1
+    document.getElementById('button3').innerHTML = buttonValues[buttonIndex - 1]
     document.getElementById("img1").src = "images/bg3.jpg";
     document.getElementById("img2").src = "images/3.jpg";
   }
