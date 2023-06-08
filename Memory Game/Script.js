25bcf27709c86e8948b91561439c197402137868
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2023-06-08 12:14:14 +0000

    testing memory

--- Memory Game/Script.js
+++ Memory Game/Script.js
@@ -1,19 +1,24 @@
-var hvlkaarten = 10
+var hvlkaarten = 10;
+var usdcards = [];
 var cardsnmbr = [];
+var selectedCardButton;
+var cardnameid;
+var selectedCardButtonold;
 
-for(var i = 1; i <= hvlkaarten; i++){
+for (var i = 1; i <= hvlkaarten; i++) {
     cardsnmbr.push(i);
 }
-console.log(cardsnmbr)
+console.log(cardsnmbr);
 
-let tempcards = cardsnmbr;
+let tempcards = cardsnmbr.slice();
 cardsnmbr = cardsnmbr.concat(tempcards);
 cardsnmbr = shuffle(cardsnmbr);
 
 function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
+    let currentIndex = array.length,
+        randomIndex;
 
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
@@ -23,11 +28,73 @@ function shuffle(array) {
     return array;
 }
 
-cardsnmbr.forEach((element) => { cardmaker(element); });
+function clickcard(cardnameid) {
+    selectedCardButton = document.getElementById(cardnameid + "btn");
 
-function cardmaker(cardname) {
-    const button = document.createElement('button');
-    button.innerText = cardname;
-    button.id = cardname+"btn";
-    document.getElementById("cards").appendChild(button);
+    if (selectedCardButton.innerText !== "" || selectedCardButton.disabled) {
+        return;
+    }
+
+    if (selectedCardButtonold) {
+        selectedCardButtonold.innerText = "";
+    }
+
+    selectedCardButton.innerText = cardnameid;
+    console.log(selectedCardButton.innerText);
+
+    if (selectedCardButtonold && selectedCardButtonold !== selectedCardButton) {
+        var correspondingCardId = selectedCardButtonold.id.replace(".1", ".2");
+        var correspondingCardButton = document.getElementById(correspondingCardId);
+
+        if (correspondingCardButton.innerText === cardnameid) {
+            selectedCardButtonold.disabled = true;
+            selectedCardButton.disabled = true;
+            correspondingCardButton.disabled = true;
+        } else if(correspondingCardButton.innerText !== cardnameid) {
+            setTimeout(function () {
+                selectedCardButton.innerText = "";
+                correspondingCardButton.innerText = "";
+            }, 500);
+        }
+
+        selectedCardButtonold = null;
+    } else {
+        selectedCardButtonold = selectedCardButton;
+    }
 }
+
+var countMap = [];
+cardsnmbr.forEach((element) => {
+    countMap[element] = (countMap[element] || 0) + 1;
+    cardmaker(element, countMap[element]);
+});
+
+function cardmaker(cardname, count) {
+    if (count > 1) {
+        var cardnameid = cardname + ".2";
+    } else {
+        var cardnameid = cardname + ".1";
+    }
+
+    const button = document.createElement('button');
+    button.onclick = function () {
+        clickcard(cardnameid);
+    };
+    button.id = cardnameid + "btn";
+    button.style.width = "130px";
+    button.style.height = "200px";
+    var div = document.getElementById("cards");
+
+    div.appendChild(button);
+}
+
+const unselectButton = document.createElement('button');
+unselectButton.onclick = function () {
+    if (selectedCardButtonold) {
+        selectedCardButtonold.innerText = "";
+        selectedCardButtonold = null;
+    }
+};
+unselectButton.innerText = "Unselect";
+var div = document.getElementById("cards");
+div.appendChild(unselectButton);
